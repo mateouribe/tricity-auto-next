d41e313c693cfda93527931c7fde2a83747018e6
Mateo Arismendy <arismendyuribemateo@gmail.com>
2024-10-24 14:43:42 -0400

    add schema.org

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -28,6 +28,73 @@ export default async function RootLayout({
           content="car financing, car loans, used cars, auto loans, tricity area, buy here pay here, used car dealership, Ontario car finance, bad credit auto loans, no credit car loans, car search, credit rebuilding, special financing programs, bankruptcy car loans, car inventory, SUVs, sedans, minivans, Ontario used cars"
         />
         <Google />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "http://schema.org",
+              "@type": "LocalBusiness",
+              name: "Used Cars Dealership in Waterloo, ON | Tricity Auto",
+              image:
+                "https://www.tricityautofinance.com/assets/images/tricity-auto-social-media-img.png",
+              telephone: "(519) 576-3421",
+              email: "mailto:tricityauto1@gmail.com",
+              url: "https://www.tricityautofinance.com",
+              address: {
+                "@type": "PostalAddress",
+                streetAddress: "109 Bridgeport Rd E",
+                addressLocality: "Waterloo, ON",
+                postalCode: "N2J 2K3",
+              },
+              openingHoursSpecification: [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: "Monday",
+                  opens: "10:00",
+                  closes: "20:00",
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: "Tuesday",
+                  opens: "10:00",
+                  closes: "20:00",
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: "Wednesday",
+                  opens: "10:00",
+                  closes: "20:00",
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: "Thursday",
+                  opens: "10:00",
+                  closes: "20:00",
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: "Friday",
+                  opens: "10:00",
+                  closes: "18:00",
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: "Saturday",
+                  opens: "10:00",
+                  closes: "17:00",
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  dayOfWeek: "Sunday",
+                  opens: "00:00",
+                  closes: "00:00",
+                  description: "Closed",
+                },
+              ],
+            }),
+          }}
+        />
       </head>
       <body>
         <Layout locale={locale} namespaces={namespaces}>
